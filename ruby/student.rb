e9e2adaf2478ab032b1303bf15cc46c97df938fc
hana <hanasabih@gmail.com>
2023-04-12 13:58:40 +0300

    set-up-associations

--- ruby/student.rb
+++ ruby/student.rb
@@ -1,10 +1,12 @@
 require_relative 'Person'
+
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
+    classroom.students << self
   end
 
   def classroom=(classroom)
